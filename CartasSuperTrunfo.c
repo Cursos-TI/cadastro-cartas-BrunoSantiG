9d978528fcf3bdc6a1558678500622ec4c6f145a
Bruno Santi Garcia <bruno.santi.98@outlook.com>
2025-11-09 17:19:31 +0000

    Realiza comparação entre carta e exibe ganhador por atributo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,6 +4,43 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
+int compararAtributos(float valor1, float valor2, int menor_vence) {
+  if (menor_vence) {
+    return valor1 < valor2;
+  } else {
+    return valor1 > valor2;
+  }
+}
+
+int recuperarCartaGanhadora(int resultado) {
+  return resultado? 1 : 2;
+}
+
+void compararCartas(unsigned long int carta1_populacao, unsigned long int carta2_populacao, float carta1_area, float carta2_area, float carta1_pib, float carta2_pib, int carta1_pontos, int carta2_pontos, float carta1_densidade, float carta2_densidade, float carta1_pib_per_capita, float carta2_pib_per_capita, float carta1_super_poder, float carta2_super_poder) {
+  printf("\n\nComparação de Cartas:\n");
+
+  int resultado_populacao = compararAtributos(carta1_populacao, carta2_populacao, 0);
+  printf("População: Carta %d venceu (%d)\n", recuperarCartaGanhadora(resultado_populacao), resultado_populacao);
+
+  int resultado_area = compararAtributos(carta1_area, carta2_area, 0);
+  printf("Área: Carta %d venceu (%d)\n", recuperarCartaGanhadora(resultado_area), resultado_area);
+
+  int resultado_pib = compararAtributos(carta1_pib, carta2_pib, 0);
+  printf("PIB: Carta %d venceu (%d)\n", recuperarCartaGanhadora(resultado_pib), resultado_pib);
+
+  int resultado_pontos = compararAtributos(carta1_pontos, carta2_pontos, 0);
+  printf("Pontos Turísticos: Carta %d venceu (%d)\n", recuperarCartaGanhadora(resultado_pontos), resultado_pontos);
+
+  int resultado_densidade = compararAtributos(carta1_densidade, carta2_densidade, 1);
+  printf("Densidade Populacional: Carta %d venceu (%d)\n", recuperarCartaGanhadora(resultado_densidade), resultado_densidade);
+
+  int resultado_pib_per_capita = compararAtributos(carta1_pib_per_capita, carta2_pib_per_capita, 0);
+  printf("PIB per Capita: Carta %d venceu (%d)\n", recuperarCartaGanhadora(resultado_pib_per_capita), resultado_pib_per_capita);
+
+  int resultado_super_poder = compararAtributos(carta1_super_poder, carta2_super_poder, 0);
+  printf("Super Poder: Carta %d venceu (%d)\n", recuperarCartaGanhadora(resultado_super_poder), resultado_super_poder);
+}
+
 float calcularDensidade(unsigned long int populacao, float area) {
   return (float)populacao / area;
 }
@@ -12,20 +49,7 @@ float calcularPibPerCapita(float pib, unsigned long int populacao) {
   return (pib * 1000000000) / (float)populacao;
 }
 
-void exibirCarta(int numero, char estado, char codigo[4], char nome[50], unsigned long int populacao, float area, float pib, int pontos, float densidade, float pib_per_capita) {
-
-  printf("\nEstado: %c", estado);
-  printf("\nCodigo: %s", codigo);
-  printf("\nNome da Cidade: %s", nome);
-  printf("Populacao: %lu", populacao);
-  printf("\nArea: %.2f km²", area);
-  printf("\nPIB: %.2f bilhões de reais", pib);
-  printf("\nNúmero de Pontos Turísticos: %d", pontos);
-  printf("\nDensidade Populacional: %.2f hab/km²", densidade);
-  printf("\nPIB per Capita: %.2f reais", pib_per_capita);
-}
-
-void lerCarta(int numero, char *estado, char codigo[4], char nome[50], unsigned long int *populacao, float *area, float *pib, int *pontos, float *densidade, float *pib_per_capita) {
+void lerCarta(int numero, char *estado, char codigo[4], char nome[50], unsigned long int *populacao, float *area, float *pib, int *pontos, float *densidade, float *pib_per_capita, float *super_poder) {
   printf("----------Carta %d----------", numero);
 
   printf("\n\nDigite o estado da cidade(A-H): ");
@@ -53,6 +77,7 @@ void lerCarta(int numero, char *estado, char codigo[4], char nome[50], unsigned 
 
   *densidade = calcularDensidade(*populacao, *area);
   *pib_per_capita = calcularPibPerCapita(*pib, *populacao);
+  *super_poder = (float)(*populacao) + *area + *pib + (float)(*pontos) + *pib_per_capita + (1 / *densidade);
 }
 
 int main() {
@@ -61,30 +86,29 @@ int main() {
   char carta2_estado, carta2_codigo[4], carta2_nome_cidade[50];
   unsigned long int carta1_populacao, carta2_populacao;
   int carta1_qtd_pontos_turisticos, carta2_qtd_pontos_turisticos;
-  float carta1_area, carta1_pib, carta1_densidade, carta1_pib_per_capita;
-  float carta2_area, carta2_pib, carta2_densidade, carta2_pib_per_capita;
+  float carta1_area, carta1_pib, carta1_densidade, carta1_pib_per_capita, carta1_super_poder;
+  float carta2_area, carta2_pib, carta2_densidade, carta2_pib_per_capita, carta2_super_poder;
 
   // Área para entrada de dados
   lerCarta(1, &carta1_estado, carta1_codigo, carta1_nome_cidade,
            &carta1_populacao, &carta1_area, &carta1_pib,
            &carta1_qtd_pontos_turisticos, &carta1_densidade,
-           &carta1_pib_per_capita);
+           &carta1_pib_per_capita, &carta1_super_poder);
   printf("\n");
   lerCarta(2, &carta2_estado, carta2_codigo, carta2_nome_cidade,
            &carta2_populacao, &carta2_area, &carta2_pib,
            &carta2_qtd_pontos_turisticos, &carta2_densidade,
-           &carta2_pib_per_capita);
+           &carta2_pib_per_capita, &carta2_super_poder);
 
-   // Área para exibição dos dados da cidade
-  exibirCarta(1, carta1_estado, carta1_codigo, carta1_nome_cidade,
-              carta1_populacao, carta1_area, carta1_pib,
-              carta1_qtd_pontos_turisticos, carta1_densidade,
-              carta1_pib_per_capita);
-  printf("\n");
-  exibirCarta(2, carta2_estado, carta2_codigo, carta2_nome_cidade,
-              carta2_populacao, carta2_area, carta2_pib,
-              carta2_qtd_pontos_turisticos, carta2_densidade,
-              carta2_pib_per_capita);
+  // Área para exibição dos dados da cidade
+  compararCartas(carta1_populacao, carta2_populacao,
+    carta1_area, carta2_area,
+    carta1_pib, carta2_pib,
+    carta1_qtd_pontos_turisticos, carta2_qtd_pontos_turisticos,
+    carta1_densidade, carta2_densidade,
+    carta1_pib_per_capita, carta2_pib_per_capita,
+    carta1_super_poder, carta2_super_poder
+  );
 
   return 0;
 }
